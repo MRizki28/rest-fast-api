49ec2ba519774ddcb1b82661a080f0109cb44a58
Rizki <muhammadrizkitkj123@gmail.com>
2025-05-14 14:02:34 +0800

    add post function

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from app.controllers.user_controller import UserController
+from app.schemas.user_schema import UserSchema
+
 class Api:
     def __init__(self):
         self.controller = UserController()
@@ -10,6 +12,10 @@ class Api:
         @self.router.get("/user")
         def get_all_data():
             return self.controller.getAllData()
+        
+        @self.router.post("/user/create")
+        def create_data(user: UserSchema):
+            return self.controller.createData(user)
         
     def get_router(self):
         return self.router
